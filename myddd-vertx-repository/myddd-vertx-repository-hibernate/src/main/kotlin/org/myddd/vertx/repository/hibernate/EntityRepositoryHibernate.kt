d307834288646b77a418afd8c02ccee16916986a
lingen.liu <lingen.liu@gmail.com>
2021-01-22 16:21:42 +0800

    feat: 完善Hibernate的逻辑

--- myddd-vertx-repository/myddd-vertx-repository-hibernate/src/main/kotlin/org/myddd/vertx/repository/hibernate/EntityRepositoryHibernate.kt
+++ myddd-vertx-repository/myddd-vertx-repository-hibernate/src/main/kotlin/org/myddd/vertx/repository/hibernate/EntityRepositoryHibernate.kt
@@ -6,38 +6,81 @@ import org.hibernate.reactive.mutiny.Mutiny
 import org.myddd.vertx.domain.Entity
 import org.myddd.vertx.repository.api.EntityRepository
 import java.io.Serializable
+import java.lang.RuntimeException
 import javax.persistence.Persistence
+
+
+
 
 open class EntityRepositoryHibernate : EntityRepository {
 
     protected val sessionFactory: Mutiny.SessionFactory by lazy { Persistence.createEntityManagerFactory("default")
         .unwrap(Mutiny.SessionFactory::class.java) }
 
-    override fun <T : Entity?> save(entity: T): Future<T> {
+    override fun <T : Entity> save(entity: T): Future<T> {
         val future = PromiseImpl<T>()
-        sessionFactory.withTransaction { session, _ ->
-            session.persist(entity).eventually {
-                future.onSuccess(entity)
+        exists(entity::class.java,entity.getId()).onSuccess { exists ->
+            if(exists) {
+                sessionFactory.withTransaction { session, _ ->
+                    session.merge(entity).invoke { merge ->
+                        future.onSuccess(merge)
+                    }
+                }.await().indefinitely()
+
+            }else{
+                sessionFactory.withTransaction { session, _ ->
+                    session.persist(entity).eventually {
+                        future.onSuccess(entity)
+                    }
+                }.await().indefinitely()
             }
-        }.await().indefinitely()
+        }
+
+
         return future
     }
 
-    override fun <T : Entity?> get(clazz: Class<T>?, id: Serializable?): Future<T?> {
+    override fun <T : Entity> get(clazz: Class<T>?, id: Serializable?): Future<T?> {
         val future = PromiseImpl<T>()
         sessionFactory.withSession { session ->
             session.find(clazz,id).invoke {
-                    user -> future.onSuccess(user)
+                    findObj -> future.onSuccess(findObj)
             }
         }.await().indefinitely()
         return future
     }
 
-    override fun <T : Entity?> exists(clazz: Class<T>?, id: Serializable?): Future<Boolean> {
+    override fun <T : Entity> exists(clazz: Class<T>?, id: Serializable?): Future<Boolean> {
         val future = PromiseImpl<Boolean>()
         sessionFactory.withSession { session ->
             session.find(clazz,id).invoke {
-                    user -> if(user != null) future.onSuccess(true) else future.onSuccess(false)
+                    findObj -> if(findObj != null) future.onSuccess(true) else future.onSuccess(false)
+            }
+        }.await().indefinitely()
+        return future
+    }
+
+    override fun <T : Entity> batchSave(entityList:Array<T>): Future<Boolean> {
+        val future = PromiseImpl<Boolean>()
+        sessionFactory.withTransaction { session, _ ->
+            session.persistAll(*entityList).eventually {
+                future.onSuccess(true)
+            }
+        }.await().indefinitely()
+        return future
+    }
+
+    override fun <T : Entity> delete(clazz: Class<T>?, id: Serializable?): Future<Boolean> {
+        val future = PromiseImpl<Boolean>()
+        sessionFactory.withTransaction { session, _ ->
+            session.find(clazz,id).chain {
+                findObj ->
+                if(findObj!= null) {
+                    session.remove(findObj).eventually { future.onSuccess(true) }
+                }else{
+                    future.complete()
+                    throw RuntimeException("Entity not found")
+                }
             }
         }.await().indefinitely()
         return future
--- myddd-vertx-repository/myddd-vertx-repository-hibernate/src/test/kotlin/org/myddd/vertx/repository/hibernate/TestEntityRepositoryHibernate.kt
+++ myddd-vertx-repository/myddd-vertx-repository-hibernate/src/test/kotlin/org/myddd/vertx/repository/hibernate/TestEntityRepositoryHibernate.kt
@@ -6,19 +6,35 @@ import io.vertx.junit5.VertxTestContext
 import io.vertx.kotlin.coroutines.await
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import java.lang.Exception
 
 @ExtendWith(VertxExtension::class)
 class TestEntityRepositoryHibernate {
 
-    private val entityRepository:EntityRepositoryHibernate = EntityRepositoryHibernate()
+    private val repository:EntityRepositoryHibernate = EntityRepositoryHibernate()
 
     @Test
     fun testAdd(vertx:Vertx, testContext: VertxTestContext){
         val user =  User(username = "lingen",age = 35)
-        entityRepository.save(user).onSuccess {
+        repository.save(user).onSuccess {
             user -> if(user.id > 0) testContext.completeNow() else testContext.failed()
+        }
+    }
+
+    @Test
+    fun testUpdate(vertx:Vertx, testContext: VertxTestContext){
+        GlobalScope.launch {
+            val user =  User(username = "lingen",age = 35)
+            val createdUser =  repository.save(user).await()
+            createdUser.age = 36
+
+            repository.save(createdUser).await()
+
+            var queryUser = repository.get(User::class.java,createdUser.id).await()
+            if(queryUser?.age == 36) testContext.completeNow() else testContext.failed()
         }
     }
 
@@ -27,12 +43,12 @@ class TestEntityRepositoryHibernate {
 
         GlobalScope.launch {
             val user =  User(username = "lingen",age = 35)
-            val createdUser =  entityRepository.save(user).await()
-            var queryUser =entityRepository.get(User::class.java,createdUser.id).await()
+            val createdUser =  repository.save(user).await()
+            var queryUser = repository.get(User::class.java,createdUser.id).await()
 
             if(queryUser == null)testContext.failed()
 
-            var notExistsUser = entityRepository.get(User::class.java,Long.MAX_VALUE).await()
+            var notExistsUser = repository.get(User::class.java,Long.MAX_VALUE).await()
             if(notExistsUser != null)testContext.failed() else testContext.completeNow()
         }
     }
@@ -41,12 +57,44 @@ class TestEntityRepositoryHibernate {
     fun testExists(vertx:Vertx, testContext: VertxTestContext){
         GlobalScope.launch {
             val user =  User(username = "lingen",age = 35)
-            val createdUser =  entityRepository.save(user).await()
-            var exists =entityRepository.exists(User::class.java,createdUser.id).await()
+            val createdUser =  repository.save(user).await()
+            var exists =repository.exists(User::class.java,createdUser.id).await()
 
             if(exists)testContext.completeNow() else testContext.failed()
         }
     }
 
+    @Test
+    fun testBatchAdd(vertx:Vertx, testContext: VertxTestContext){
+        val users = ArrayList<User>()
+        for (i in 1..10){
+            users.add(User(username = "lingen_${i}",age = 35 + i))
+        }
+
+        val userArray:Array<User> = users.toTypedArray()
+        repository.batchSave(userArray).onSuccess { success -> if (success) testContext.completeNow() else testContext.failed() }
+    }
+
+    @Test
+    fun testDelete(vertx:Vertx, testContext: VertxTestContext){
+        GlobalScope.launch {
+            try {
+                repository.delete(User::class.java,Long.MAX_VALUE).onFailure { exception -> println(exception) }.await()
+            }catch (e:Exception){
+
+            }
+
+
+            val user =  User(username = "lingen",age = 35)
+            val createdUser =  repository.save(user).await()
+            var exists = repository.exists(User::class.java,createdUser.id).await()
+            if(!exists)testContext.failed()
+
+            repository.delete(User::class.java,createdUser.id).await()
+            exists = repository.exists(User::class.java,createdUser.id).await()
+            if(!exists)testContext.completeNow() else testContext.failed()
+
+        }
+    }
 
 }
