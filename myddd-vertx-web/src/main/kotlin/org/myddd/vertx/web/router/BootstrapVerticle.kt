758c32a51daf9794bf1094de090ed421045f27f8
lingen.liu <lingen.liu@gmail.com>
2021-04-12 17:40:42 +0800

    feat: 禁用Vert.x的DNS解析逻辑

--- myddd-vertx-web/src/main/kotlin/org/myddd/vertx/web/router/BootstrapVerticle.kt
+++ myddd-vertx-web/src/main/kotlin/org/myddd/vertx/web/router/BootstrapVerticle.kt
@@ -6,6 +6,7 @@ import io.vertx.core.Future
 import io.vertx.core.Vertx
 import io.vertx.core.http.HttpServer
 import io.vertx.core.impl.logging.LoggerFactory
+import io.vertx.core.spi.resolver.ResolverProvider.DISABLE_DNS_RESOLVER_PROP_NAME
 import io.vertx.ext.web.Router
 import io.vertx.kotlin.coroutines.CoroutineVerticle
 import io.vertx.kotlin.coroutines.await
@@ -27,6 +28,10 @@ abstract class BootstrapVerticle(private val port:Int = 8080) : CoroutineVerticl
             initIOC()
             it.complete()
         }.await()
+
+        //禁用Vert.x的DNS解析逻辑
+        System.getProperties().setProperty(DISABLE_DNS_RESOLVER_PROP_NAME,"true")
+
         initHttpServer().await()
         logger.info("Started in port: $startedPort")
     }
