c31609b54b373931a8e16c72d1b6641e45c9fd2b
lingen.liu <lingen.liu@gmail.com>
2021-07-23 11:20:10 +0800

    feat: 升级vert.x版本至4.1.1，升级hibernate-reactive至CR8，升级Kotlin至1.5.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,22 +4,24 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     java
     `maven-publish`
-    kotlin("jvm") version "1.5.20"
+    kotlin("jvm") version "1.5.21"
     id("jacoco")
     id("org.sonarqube") version "3.0"
 }
 
-val projectVersion = "1.1.0-SNAPSHOT"
+val projectVersion = "1.2.0-SNAPSHOT"
 
 extra["version"] = projectVersion
-extra["vertx_version"] = "4.1.0"
+
+extra["vertx_version"] = "4.1.1"
+extra["hibernate_reactive_version"] = "1.0.0.CR8"
+
 extra["log4j_version"] = "2.14.0"
 extra["jackson_version"] = "2.12.1"
 extra["javax_persistence_version"] = "2.2.1"
 extra["mockito_version"] = "3.7.7"
 extra["commons_lang3_version"] = "3.12.0"
 extra["junit5_version"] = "5.7.1"
-extra["hibernate_reactive_version"] = "1.0.0.CR7"
 
 group = "org.myddd.vertx"
 version = projectVersion
