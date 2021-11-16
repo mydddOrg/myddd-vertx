82d33bca5e93ae5bb5e7924dc1bbd09a77c084d5
lingen liu <lingen.liu@gmail.com>
2021-11-16 17:05:25 +0800

    fix: 修复一些小问题

--- myddd-vertx-base/myddd-vertx-base-api/src/main/kotlin/org/myddd/vertx/base/BusinessLogicException.kt
+++ myddd-vertx-base/myddd-vertx-base-api/src/main/kotlin/org/myddd/vertx/base/BusinessLogicException.kt
@@ -3,6 +3,6 @@ package org.myddd.vertx.base
 import org.myddd.vertx.error.ErrorCode
 import java.lang.RuntimeException
 
-class BusinessLogicException(val errorCode:ErrorCode,val values: Array<String> = emptyArray()) : RuntimeException(errorCode.toString()) {
+open class BusinessLogicException(val errorCode:ErrorCode,val values: Array<String> = emptyArray()) : RuntimeException(errorCode.toString()) {
 
 }
