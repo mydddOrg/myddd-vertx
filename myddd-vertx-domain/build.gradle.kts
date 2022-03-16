b1a6012cb1b118778f1198ad5269a2f244b39316
Lingen Liu <lingen.liu@gmail.com>
2022-03-16 17:13:02 +0800

    chore: 将mutiny依赖修改为domain层的核心依赖

--- myddd-vertx-domain/build.gradle.kts
+++ myddd-vertx-domain/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
     api(project(":myddd-vertx-base:myddd-vertx-base-api"))
     api("org.eclipse.persistence:javax.persistence:${rootProject.extra["javax_persistence_version"]}")
     api("com.fasterxml.jackson.core:jackson-annotations:${rootProject.extra["jackson_version"]}")
-    implementation("io.smallrye.reactive:mutiny:${rootProject.extra["mutiny-version"]}")
+    api("io.smallrye.reactive:mutiny:${rootProject.extra["mutiny-version"]}")
 
     testImplementation(project(":myddd-vertx-base:myddd-vertx-base-provider"))
     testImplementation("com.fasterxml.jackson.core:jackson-databind:${rootProject.extra["jackson_version"]}")
